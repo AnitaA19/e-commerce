876a6ebe492975d2d41f6596026bd9a9c7744e7a
ANITA\anita <balasanyananita@gmail.com>
2025-03-10 22:44:20 +0400

    fix: fixed button

--- src/components/CartSVG.jsx
+++ src/components/CartSVG.jsx
@@ -2,6 +2,7 @@ import styles from "./Cart.module.css"
 
 function CartSVG() {
     return (
+        <button data-testid='add-to-cart'>
           <svg
                         width={24}
                         height={24}
@@ -23,6 +24,7 @@ function CartSVG() {
                             fill="#43464E"
                         />
                     </svg>
+                    </button>
     )
 }
 
