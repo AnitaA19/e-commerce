be926cf2ce2cd7549f05a4a74b46d64ad9c4296c
ANITA\anita <balasanyananita@gmail.com>
2025-03-12 21:02:31 +0400

    fix: fixed add to cart

--- src/constants.js
+++ src/constants.js
@@ -1,2 +1,2 @@
 export const DESCRIPTIONCHARLIMIT = 200; 
-export const API_URL = 'https://corsproxy.io/?http://backend-ecommerce.atwebpages.com/index.php';
+export const API_URL = 'https://api.allorigins.win/raw?url=http://backend-ecommerce.atwebpages.com/index.php';
